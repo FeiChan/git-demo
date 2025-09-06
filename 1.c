138f98bf501c9f40b1923d16c57ca3fbf61f255a
cf <cf@localhost.localdomain>
2025-09-06 09:43:41 -0400

    add accounting.c file

--- 1.c
+++ 1.c
@@ -1,7 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-	printf("helo,world!");
-	return 0;
-}
+int main(void) {
+	    int s, d;
+
+	        // Keep reading pairs of integers until EOF
+		while (~scanf("%d%d", &s, &d)) {
+		printf("Read values: s = %d, d = %d\n", s, d);
+		}
+		
+		printf("End of input detected.\n");
+		return 0;
+		}
+		
+	    
